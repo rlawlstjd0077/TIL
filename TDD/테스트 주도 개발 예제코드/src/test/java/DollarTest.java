94fa2e76a378391004a589d06b47f8f84fdbc820
rlawlstjd0077 <rlawlstjd077@gmail.com>
2020-02-05 10:00:54 +0900

    Test 코드 수정

--- TDD/테스트 주도 개발 예제코드/src/test/java/DollarTest.java
+++ TDD/테스트 주도 개발 예제코드/src/test/java/DollarTest.java
@@ -13,18 +13,18 @@ public class DollarTest {
   @Test
   public void testMultiplication() {
     Money five = Money.dollar(5);
-    assertEquals(new Dollar(10), five.times(2));
-    assertEquals(new Dollar(15), five.times(3));
+    assertEquals(Money.dollar(10), five.times(2));
+    assertEquals(Money.dollar(15), five.times(3));
   }
 
 
   @Test
   public void testEquality() {
-    assertTrue(new Dollar(5).equals(new Dollar(5)));
-    assertFalse(new Dollar(5).equals(new Dollar(6)));
-    assertTrue(new Franc(5).equals(new Franc(5)));
-    assertFalse(new Franc(5).equals(new Franc(6)));
-    assertFalse(new Dollar(5).equals(new Franc(5)));
+    assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+    assertTrue(Franc.franc(5).equals(Franc.franc(5)));
+    assertFalse(Franc.franc(5).equals(Franc.franc(6)));
+    assertFalse(Money.dollar(5).equals(Franc.franc(5)));
   }
 
   @Test
