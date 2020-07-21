da2247422f066f8366717ca91e9d2e0fbe02d66e
rlawlstjd0077 <rlawlstjd077@gmail.com>
2020-07-21 09:20:09 +0900

    스프링 부트 시큐리티 프로젝트 추가

---
+++ Spring/스프링 부트 개념과 활용/4. 스프링부트 활용/untitled/src/main/java/com/jinseong/soft/HelloController.java
@@ -0,0 +1,17 @@
+package com.jinseong.soft;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/my")
+    public String my() {
+        return "my";
+    }
+}
---
+++ Spring/스프링 부트 개념과 활용/4. 스프링부트 활용/untitled/src/test/java/com/jinseong/soft/HelloControllerTest.java
@@ -0,0 +1,43 @@
+package com.jinseong.soft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.thymeleaf.spring5.processor.SpringActionTagProcessor;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void test() throws Exception {
+        mockMvc.perform(get("/hello").accept(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"));
+    }
+
+    @Test
+    @WithMockUser
+    public void my() throws Exception {
+        mockMvc.perform(get("/my"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("my"));
+    }
+}
