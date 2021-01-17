edfb85ad90d0fcad304215fa6d38788b32708bbe
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-01-17 15:07:07 +0900

    Java Project 추가

---
+++ Java/JavaProject/src/main/java/com/jinseong/soft/Application.java
@@ -0,0 +1,10 @@
+package com.jinseong.soft;
+
+public class Application {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String str1 = new String("Hello789");
+        //System.out.println(System.identityHashCode(str));
+        //System.out.println(System.identityHashCode(str1));
+    }
+}
